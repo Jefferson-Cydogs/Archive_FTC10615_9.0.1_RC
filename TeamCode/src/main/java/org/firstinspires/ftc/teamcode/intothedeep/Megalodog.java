6dfdd482e4962065d39c047f4565036a0ac04eb3
Jefferson-Cydogs <jeffersoncydogs@gmail.com>
2024-10-07 19:40:52 -0400

    Ideas for robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/Megalodog.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.intothedeep;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.core.MegalodogChassis;
+
+class Megalodog extends MegalodogChassis {
+    private int topBucket = 2000;
+    private int lowBucket = 1000;
+    private int highBar = 1500;
+    private int lowBar = 500;
+    private double Gripper = 105.8;
+    private int highChamber = 1200;
+    private int lowChamber = 255;
+    private int deliveryBox = 105;
+    private Servo Intake;
+
+
+    public void TurnIntakeOn () {
+//It turns the continues servo forward
+
+    }
+    public void TurnIntakeOff () {
+//It turns the continues servo off
+
+    }
+    public void GrabSpeicen (int waitime) {
+//The servo rotates forward
+
+    }
+    public void HookAndLetGo (int waitime) {
+// It pushes the sepiecem down and then lets go of it
+
+    }
+
+
+
+
+
+}
